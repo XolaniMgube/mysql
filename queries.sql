b427ed9a9b5f58715fd67e47283a14d832d49413
XolaniMgube <xnmgube@gmail.com>
2020-04-15 15:33:07 +0200

    Tested on adminer

--- queries.sql
+++ queries.sql
@@ -1,70 +1,70 @@
 -- Select everything from the customer table
-select * from customers
+select * from customers;
 
 
 -- Select the firstname column from the customer table
-select firstname from customers
+select firstname from customers;
 
 
 -- Select the firstname with the id of 1 from customer table
-select firstname from customers where customerid = 1
+select firstname from customers where customerid = 1;
 
 
 -- Updating names for a row in the customer table
-update customers set firstname = 'lerato', lastname = 'mabitso' where customerid = 1
+update customers set firstname = 'lerato', lastname = 'mabitso' where customerid = 1;
 
 
 -- deleting the row with id = 2 from customer table
-delete from customers where customerid = 2
+delete from customers where customerid = 2;
 
 
 -- Select the number of times "not shipped" and "shipped" appear on the orders table
-select count(*) from orders where status = 'Not shipped' 
-select count(*) from orders where status = 'Shipped'
+select count(*) from orders where status = 'Not shipped' ;
+select count(*) from orders where status = 'Shipped';
 
 
 -- Select biggest value from amount column in the payments table
-select max(amount) from payments
+select max(amount) from payments;
 
 
 -- Order the rows in ascending order of the country column in the customers table
-select * from customers order by country
+select * from customers order by country;
 
 
 -- Selecting the range amounts from the buyprice column
-select * from products where buyprice between 100 and 600
+select * from products where buyprice between 100 and 600;
 
 
 -- Select everything from customers table and filter bu country and city columns
-select * from customers where country = 'Germany' and city = 'Berlin'
+select * from customers where country = 'Germany' and city = 'Berlin';
 
 
 -- Select everything from customers table and filter bu country and city columns
-select * from customers where city = 'Durban' or city = 'Cape Town'
+select * from customers where city = 'Durban' or city = 'Cape Town';
 
 
 -- Select everything greater than 500 from the products table
-select * from products where buyprice > 500
+select * from products where buyprice > 500;
 
 
 -- Select the sum from the amounts column in the payments table
-select sum(amount) from payments
+select sum(amount) from payments;
 
 
 -- Select the number of times shipped appeared on the orders table
-select count(*) from orders where status = 'Shipped'
+select count(*) from orders where status = 'Shipped';
 
 
 -- Select the average of the buyprice column both in rands and dollars
-In rands: select avg(buyprice) from products
-In dollars: select avg(buyprice)/12 from products
+select avg(buyprice) from products;
+select avg(buyprice)/12 from products;
 
 
 -- Joining the payments and customer table in reference from the customer id
 select * from customers
-join payments on customers.customerid = payments.customerid
+join payments on customers.customerid = payments.customerid;
 
 
 -- Select  everything from products where description in filtered
 select * from products
-where description like '%Turnable front wheels%'
+where description like '%Turnable front wheels%';
--- tables.sql
+++ tables.sql
@@ -1,5 +1,3 @@
-
-
 -- Creating the Customer Table
 create table Customers(
   customerID serial primary key,
@@ -11,10 +9,7 @@ create table Customers(
   Email varchar(100) not null,
   City varchar(20) not null,
   Country varchar(50) not null
-)
-
-
-
+);
 
 -- Creating the Employees Table
 create table Employees(
@@ -23,9 +18,23 @@ create table Employees(
   LastName varchar(50) not null,
   Email varchar(100) not null,
   JobTitle varchar(20)
-)
+);
 
+-- Creating the Payments Table
+create table Payments(
+  customerID int references customers(customerID),
+  PaymentID serial primary key,
+  PaymentDate date not null,
+  Amount decimal not null
+);
 
+-- Creating the Products Table
+create table products(
+  ProductID serial primary key,
+  ProductName varchar(100) not null,
+  Description varchar(300) not null,
+  BuyPrice decimal not null
+);
 
 -- Creating the Orders Table
 create table Orders(
@@ -37,139 +46,90 @@ create table Orders(
   DateRequired date not null,
   DateShipped date,
   Status varchar(20) not null
+);
+
+-- Inserting rows into customers
+insert into Customers(
+  FirstName,
+  LastName,
+  Gender,
+  Address,
+  Phone,
+  Email,
+  City,
+  Country
 )
-
-
-
--- Creating the Payments Table
-create table Payments(
-  customerID int references customers(customerID),
-  PaymentID serial primary key,
-  PaymentDate date not null,
-  "Amount(R)" decimal not null
-)
-
-
--- Creating the Products Table
-create table products(
-  ProductID serial primary key,
-  ProductName varchar(100) not null,
-  Description varchar(300) not null,
-  "BuyPrice (R)" decimal not null
-)
-
-
-
--- Inserting rows into Customers
-insert into customers (
-  firstname,
-  lastname,
-  gender,
-  address,
-  phone,
-  email,
-  city,
-  country
-) values(
-  'John',
-  'Hibert',
-  'Male',
-  '284 chaucer st',
-  '084789657',
-  'john@gmail.com',
-  'Johannesburg',
-  'South Africa"'
-)
-
+values
+  ('John', 'Hibert', 'Male', '284 Chaucer st', '084789657', 'john@gmail.com', 'Johannesburg', 'South Africa'),
+  ('Thando', 'Sithole', 'Female', '204 Sec 1', '079444558', 'thando@gmail.com', 'Capetown', 'South Africa'),
+  ('Leon', 'Glen', 'Male', '81 Everton Rd, Gillits', '082083283', 'leon@gmail.com', 'Durban', 'South Africa'),
+  ('Charl', 'Muller', 'Male', '290A Dorset Ecke', '+44856872553', 'charl.mullar@yahoo.com', 'Berlin', 'Germin'),
+  ('Julia', 'Stein', 'Female', '2 Wernerring', '+448672445058', 'js234@yahoo.com', 'Frankfurt', 'Germany');
 
 
 -- Inserting rows into employees
-insert into employees (
-  firstname,
-  lastname,
-  email,
-  jobtitle
-) 
-values(
-  'Kani',
-  'Matthew',
-  'mat@gmail.com',
-  'Manager'
+insert into Employees(
+    FirstName,
+    LastName,
+    Email,
+    JobTitle)
+values
+    ('Kani', 'Mattew', 'mat@gmail.com', 'Manager'),
+    ('Lessly', 'Cronje', 'lesc@gmail.com', 'Clerk'),
+    ('Gideon', 'Maduku', 'm@gmail.com', 'Accountant');
+
+
+
+-- Inserting rows into products
+insert into Products(
+    ProductName,
+    Description,
+    BuyPrice
 )
+values
+    ('Harley Davidson Chopper', 'This replica features working kickstand, front suspension, gear-shift lever', 150.75),
+    ('Classic Car', 'Turnable front wheels, steering function', 550.75),
+    ('Sports Car', 'Turnable front wheels, steering function', 700.60);
 
 
-
--- inserting rows into the Orders Table
-insert into payments(
-paymentdate,
-amount
-)values(
-'01-09-2018',
-150.75
-),
-(
-'03-09-2018',
-150.75
-),
-(
-'03-09-2018',
-700.60
+-- Inserting rows into payments
+insert into Payments(
+    PaymentDate,
+    Amount
 )
+values
+    ('01/09/2018', 150.75),
+    ('03/09/2018', 150.75),
+    ('03/09/2018', 700.60);
 
 
-
--- Inserting data into the Payments table
-insert into payments(
-paymentdate,
-amount
-)values(
-'01-09-2018',
-150.75
-),
-(
-'03-09-2018',
-150.75
-),
-(
-'03-09-2018',
-700.60
-);
-
-
-
--- Inserting data into the Products table
-insert into products(
-productname,
-description,
-buyprice
-)values(
-'Classic Car',
-'Turnable front wheels, steering function',	
-550.75
-),
-(
-'Sports car',
-'Turnable front wheels, steering function',
-700.60
-);
-
+-- Inserting rows into orders
+insert into Orders(
+    DateRequired,
+    DateShipped, 
+    Status
+)
+values
+    ('05/09/2018', NULL, 'Not Shipped'),
+    ('04/09/2018', '03/09/2018', 'Shipped'),
+    ('06/09/2018', NULL, 'Not Shipped');
 
 
 -- Updating the Payments table
-update payments set customerid = 1 where paymentid = 1
-update payments set customerid = 5 where paymentid = 2
-update payments set customerid = 4 where paymentid = 3
+update payments set customerid = 1 where paymentid = 1;
+update payments set customerid = 5 where paymentid = 2;
+update payments set customerid = 4 where paymentid = 3;
 
 
 -- Updating the orders table
-update orders set productid = 1 where orderid = 1
-update orders set productid = 1 where orderid = 2
-update orders set productid = 3 where orderid = 3
+update orders set productid = 1 where orderid = 1;
+update orders set productid = 1 where orderid = 2;
+update orders set productid = 3 where orderid = 3;
 
-update orders set paymentid = 1 where orderid = 1
-update orders set paymentid = 2 where orderid = 2
-update orders set paymentid = 3 where orderid = 3
+update orders set paymentid = 1 where orderid = 1;
+update orders set paymentid = 2 where orderid = 2;
+update orders set paymentid = 3 where orderid = 3;
 
-update orders set fulfilledbyemployeeid = 2 where paymentid = 1
-update orders set fulfilledbyemployeeid = 2 where orderid = 2
-update orders set fulfilledbyemployeeid = 3 where orderid = 3
+update orders set fulfilledbyemployeeid = 2 where paymentid = 1;
+update orders set fulfilledbyemployeeid = 2 where orderid = 2;
+update orders set fulfilledbyemployeeid = 3 where orderid = 3;
